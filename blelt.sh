6397e8901f4f3694bd773ffacc6f2bb7c30ccf97
Maghiel Dijksman <mail@mdijksman.nl>
2025-08-05 23:27:41 +0200

    refactor rename ble-lt-thermometer.py to blelt2mqtt.py

--- blelt.sh
+++ blelt.sh
@@ -7,5 +7,5 @@ MY_PWD="$( dirname -- "$( readlink -f -- "$0"; )" )";
 
 while true; do
 sleep 30
-{ python3 "$MY_PWD/ble-lt-thermometer.py" > "$MY_PWD/blelt.logs"; } 2>&1
+{ python3 "$MY_PWD/blelt2mqtt.py" > "$MY_PWD/blelt.logs"; } 2>&1
 done
